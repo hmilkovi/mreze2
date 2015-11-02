1dea2e5e0508a9f6a151921a841ca938d2aa226b
hmilkovi <hmilkovi@gmail.com>
2015-11-02 19:41:03 +0100

    remove comments

--- labosi4/server.c
+++ labosi4/server.c
@@ -16,8 +16,7 @@ struct tm * timeinfo;
 
 char* concat(char *s1, char *s2)
 {
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
-    //in real code you would check for errors in malloc here
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
     strcpy(result, s1);
     strcat(result, s2);
     return result;
